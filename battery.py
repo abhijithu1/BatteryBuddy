a821968383d09ccbad152c49e00db5089b1ddb13
Abhijith.U <79012764+abhijithu1@users.noreply.github.com>
2024-01-13 16:23:28 +0530

    first commit

---
+++ battery.py
@@ -0,0 +1,16 @@
+import psutil as ps
+import time
+from playsound import playsound
+from pycaw.pycaw import AudioUtilities,ISimpleAudioVolume
+# from pynput.keyboard import Key,Controller
+# def volumeset():
+#     currentvolume = AudioUtilities.
+while True:
+    battery = ps.sensors_battery()
+    status = battery.power_plugged
+    charge = battery.percent
+    if(status == False and charge <= 50):
+        playsound("batt.mp3")
+        # C:\\Users\\abhiu\\Desktop\\python_pro\\
+        time.sleep(660)
+    time.sleep(60)
